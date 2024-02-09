af110ad80dca084384a09f9928ed997b59020cc4
Kamyar jafari <kamyarjafari3000@gmail.com>
2024-02-09 08:28:50 +0330

    add first-project-kj

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Neon from "./components/Neon";
+
+const App = () => {
+  return(
+    <div>
+      <Neon />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Neon.jsx
@@ -0,0 +1,42 @@
+import "../App.css"
+
+const Neon = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="#" className="a">
+                        Reactjs
+                    </a>
+                </li>
+                <li>
+                    <a className="Belief" href="#">
+                        Belief
+                    </a>
+                </li>
+                <li>
+                    <a className="Build" href="#">
+                        Build
+                    </a>
+                </li>
+                <li>
+                    <a className="Try" href="#">
+                        Try
+                    </a>
+                </li>
+                <li>
+                    <a className="Ideas" href="#">
+                        Ideas
+                    </a>
+                </li>
+                <li>
+                    <a className="Successs" href="#">
+                        Success
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Neon;
